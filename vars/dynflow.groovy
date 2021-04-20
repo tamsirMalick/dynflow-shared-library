a5663b394ab5efa4cdbbe5a42a5ccf18a77b7d9f
tamsirMalick <malickmbengue95@hotmail.fr>
2021-04-20 11:30:36 +0000

    try call curl

--- vars/dynflow.groovy
+++ vars/dynflow.groovy
@@ -1,4 +1,4 @@
-def get(){
-    String getResult = new URL('http://dynflow-gateway-dacdynflow-dev.k8s-test.orange-sonatel.com/api/build-microservice/builds').text
-    println(getResult);
+def call() {
+        String getResult = new URL('http://dynflow-gateway-dacdynflow-dev.k8s-test.orange-sonatel.com/api/build-microservice/builds').text
+        println(getResult);
 }
