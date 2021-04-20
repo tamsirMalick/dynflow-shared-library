b34b5391f1211a8ccb056085d09d4b4b23261d76
Malick MBENGUE [SNT DSI/DAC/COE] <malickmbengue95@hotmail.fr>
2021-04-20 12:11:27 +0000

    try call curl

--- vars/dynflow.groovy
+++ vars/dynflow.groovy
@@ -1,4 +1,4 @@
 def call() {
-        String getResult = new URL('https://dynflow-gateway-dacdynflow-dev.k8s-test.orange-sonatel.com/api/build-microservice/builds').text
+        String getResult = new URL('http://gateway-dacdynflow-dev.k8s-test.orange-sonatel.com/api/build-microservice/builds').text
         println(getResult);
 }
