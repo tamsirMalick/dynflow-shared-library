6a439db165624a247f6ff6e037c40f057a2db35c
Malick MBENGUE [SNT DSI/DAC/COE] <malickmbengue95@hotmail.fr>
2021-04-20 15:08:35 +0000

    methode pour appeler l'api rest

--- vars/dynflowCall.groovy
+++ vars/dynflowCall.groovy
@@ -1,33 +1,72 @@
-static def call() {
+static def call(String env) {
+    postJenkinsFileInfo(env)
+}
+
+def postJenkinsFileInfo(String environment) {
+    println "Information du build en cours d'envois..."
     int statusCode
-    HttpURLConnection connection
-    URL newUrl
-    newUrl = new URL("http://gateway-dacdynflow-dev.k8s-test.orange-sonatel.com/api/build-microservice/builds")
-    connection = (HttpURLConnection) newUrl.openConnection()
+    String postUrl = "http://gateway-dacdynflow-dev.k8s-test.orange-sonatel.com/api/build-microservice/builds"
+    URL gatewayUrl = new URL(postUrl)
+    HttpURLConnection connection = (HttpURLConnection) gatewayUrl.openConnection()
     connection.setRequestMethod("POST")
     connection.setDoOutput(true)
+    connection.setDoInput(true)
     connection.setRequestProperty("Content-Type", "application/json")
-    String message = '{\n' +
-            '    "buildID": "Gateway",\n' +
-            '    "projectID": "MID",\n' +
-            '    "groupID": "sn.sonatel.dsi.dac.coe",\n' +
-            '    "artefactID": "OEM",\n' +
-            '    "pipelineName": "jenkins",\n' +
-            '    "appVersion": "2",\n' +
-            '    "stageName": "Build and Push Dokcer",\n' +
-            '    "stageType": "DEPLOYMENT",\n' +
-            '    "status": "SUCCESS",\n' +
-            '    "stageCommand": "sh docker -t build .",\n' +
-            '    "gitBranch": "65456dsbhjs",\n' +
-            '    "gitRevision": "lundi",\n' +
-            '    "gitResume": "12hj",\n' +
-            '    "changeAuthor": "Bitty Diouf",\n' +
-            '    "gitCommit": "THis is a test",\n' +
-            '    "gitAuthorName": "MBENGUE 3-5"\n' +
-            '}'
-    OutputStream os = connection.getOutputStream()
-    byte[] input = message.getBytes("utf-8");
-    os.write(input, 0, input.length);
-    statusCode = connection.responseCode
-    os.close()
+
+    String response = ""
+    OutputStream os
+    try {
+        os = connection.getOutputStream()
+        byte[] input = environment.getBytes("utf-8");
+        os.write(input, 0, input.length);
+
+        statusCode = connection.responseCode
+        println "Connection status code: $statusCode"
+        if (statusCode == 200) {
+            println "Authentication succeeded"
+            println "Server response:"
+            println "-----"
+            response = displayServerResponse(connection)
+            println "-----"
+        }
+        if (statusCode == 400) {
+            println "Bad request"
+            println "Server response:"
+            println "-----"
+            response = displayServerResponse(connection)
+            println "-----"
+        }
+    } catch (Exception e) {
+        println "Un problem est survenu lors de l'envois des informations"
+        println e.getMessage()
+    } finally {
+        if (connection != null) {
+            connection.disconnect();
+        }
+        if (os != null) {
+            os.close()
+        }
+    }
+    return response
+}
+
+def displayServerResponse(HttpURLConnection connection) {
+    InputStream is;
+    if (connection.getResponseCode() == 200) {
+        is = connection.getInputStream();
+    } else {
+        is = connection.getErrorStream();
+    }
+    println "Response Content-Type:" + connection.getContentType()
+    if (connection.getContentType().contains("application/json")) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        br.close();
+        println sb
+        return sb.toString()
+    }
 }
