310f87960ca3eb6e6560f8359f6707ed96b83d52
tamsirMalick <malickmbengue95@hotmail.fr>
2021-04-20 10:59:27 +0000

    call curl

---
+++ vars/dynflow.groovy
@@ -0,0 +1,4 @@
+def call(){
+    curl -X GET --header 'Accept: */*' https//://dynflow-gateway-dacdynflow-dev.k8s-test.orange-sonatel.com/api/build-microservice/builds
+
+}
--- vars/windows.groovy
+++ vars/windows.groovy
@@ -1,5 +1,5 @@
 def call(Closure body) {
-    node('windows'){
+    stage('windows'){
         body()
     }
 }
